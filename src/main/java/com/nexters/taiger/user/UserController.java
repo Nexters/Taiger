76195f089417357fbecd7a5910cb92d6bf592691
BoBinLee <globaldev@coupang.com>
2016-01-25 09:34:26 +0900

    guava 라이브러리 추가, 유저 condition 추가

--- src/main/java/com/nexters/taiger/user/UserController.java
+++ src/main/java/com/nexters/taiger/user/UserController.java
@@ -1,16 +1,26 @@
 package com.nexters.taiger.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created on Baek on 2016. 1. 23...
  */
+
+@Slf4j
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
 	@Autowired
 	private UserService userService;
+
+	@RequestMapping("/register")
+	public String register(@RequestBody UserCondition condition){
+		log.info("register : " + condition.toString());
+		return "success";
+	}
 
 }
